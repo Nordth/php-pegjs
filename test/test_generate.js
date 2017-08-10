a7776d2e341ef32ecc92ea8d5cadd516c271d96b
Elantcev Mikhail <emo-name@hotmail.com>
2017-08-10 17:31:21 +0400

    Add test/test_generate.js

---
+++ test/test_generate.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var pegjs = require('pegjs');
+var phppegjs = require('../src/phppegjs.js');
+
+function generateParser(input_file, output_file, classname)
+{
+    fs.readFile(input_file, function (err, data) {
+        if (err) throw err;
+
+        var parser = pegjs.buildParser(data.toString(),
+            {
+                cache: true,
+                plugins: [phppegjs],
+                phppegjs: {parserNamespace: 'Parser', parserClassName: classname}
+            });
+        fs.writeFile(output_file, parser);
+    });
+
+}
+
+if (!fs.existsSync('output')) fs.mkdirSync('output');
+
+for (var classname in examples)
+{
+    generateParser('../examples/' + examples[classname],
+                   "output/" + examples[classname].replace(/\.[^/.]+$/, ".php"),
+                   classname);
+}
+
